4211d5bb18abaf6ebc1f2aadc066c36b14718a4f
Maestro <akievaleksandr@mail.ru>
2022-11-08 12:59:52 +0500

    fix: fix 03 task - show files in folder without files in folder in folder

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -13,22 +13,6 @@ fs.readdir(`${path}`, { encoding: "utf-8", withFileTypes: true }, (error, dirEnt
             if (dirent_1.isFile()) {
                 direntArray = dirent_1.name.split(".");
                 console.log(direntArray[0] + " - " + direntArray[1] + " - " + stats.size * 0.001 + "kb");
-            } else {
-                fs.readdir(`${path}${dirent_1.name}`, { encoding: "utf-8", withFileTypes: true }, (error, dirEntryList) => {
-
-                    if (error) throw error;
-                    dirEntryList.forEach(dirent => {
-                        fs.stat(`${path}${dirent_1.name}/${dirent.name}`, function (err, stats) {
-                            if (dirent.isFile()) {
-                                direntArray = dirent.name.split(".");
-                                if (direntArray[1] === "gitkeep") {
-                                    direntArray[0] = "emptyFile";
-                                }
-                                console.log(direntArray[0] + " - " + direntArray[1] + " - " + stats.size * 0.001 + "kb");
-                            };
-                        });
-                    });
-                });
             };
         });
     });
