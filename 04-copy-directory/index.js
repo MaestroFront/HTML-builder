4b5e9b3daddfa35a706a51a171295134ac3ee7de
Maestro <akievaleksandr@mail.ru>
2022-11-08 20:12:58 +0500

    fix: add path.join on '04 task'

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,51 +1,40 @@
 const fs = require("fs");
+const path = require("path");
+const folder = path.join(__dirname, "files");
+const newFolder = path.join(__dirname, "files-copy");
 
-function createNewFolder(folderName) {
-
-    fs.access("./04-copy-directory/files", fs.F_OK, (err) => {
-        if (!err) {
-            fs.mkdir(`./04-copy-directory/${folderName}`, err => {});
-        }
+function createNewFolder() {
+    fs.access(folder, fs.F_OK, (err) => {
+        if (!err) fs.mkdir(newFolder, err => {});
     });
     console.log("'files-copy' created!");
-
 };
 
-async function deleteFiles(path) {
-
-    fs.readdir(path, { encoding: "utf-8", withFileTypes: true }, (error, dirEntryList) => {
+function deleteFiles() {
+    fs.readdir(`${newFolder}/`, { encoding: "utf-8", withFileTypes: true }, (error, dirEntryList) => {
         if (!error) {
             dirEntryList.forEach(dirent => {
-
-                fs.unlink(`${path}${dirent.name}`, err => {
+                fs.unlink(`${newFolder}/${dirent.name}`, err => {
                     if (err) throw err;
                 });
-
             });
         }
     });
     console.log("'files-copy' is cleared!");
-
 };
 
-function copyDir(pathFrom, pathTo) {
-
-    fs.readdir("./04-copy-directory/files", { encoding: "utf-8", withFileTypes: true }, (error, dirEntryList) => {
-
+function copyDir() {
+    fs.readdir(folder, { encoding: "utf-8", withFileTypes: true }, (error, dirEntryList) => {
         if (error) console.log(error);
         dirEntryList.forEach(item => {
-
-            fs.copyFile(`${pathFrom}${item.name}`, `${pathTo}${item.name}`, err => {
+            fs.copyFile(`${folder}/${item.name}`, `${newFolder}/${item.name}`, err => {
                 if (err) console.log(err);
             });
-
         });
-
     });
     console.log("Files coppied!");
-
 };
 
-createNewFolder("files-copy");
-deleteFiles("./04-copy-directory/files-copy/");
-copyDir("./04-copy-directory/files/", "./04-copy-directory/files-copy/");
+createNewFolder();
+deleteFiles();
+copyDir();
