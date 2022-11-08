f2f6a76dc5ce67005f44fb5c165cae277e6fbb83
Maestro <akievaleksandr@mail.ru>
2022-11-08 18:16:21 +0500

    fix: fix functions '04 task'

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,40 +1,51 @@
 const fs = require("fs");
 
 function createNewFolder(folderName) {
-    fs.mkdir(`./04-copy-directory/${folderName}`, err => {
-        if (err) {
-            console.log("The folder has already been created!");
-        } else {
-            console.log(`Created folder "${folderName}" is successfully!`);
+
+    fs.access("./04-copy-directory/files", fs.F_OK, (err) => {
+        if (!err) {
+            fs.mkdir(`./04-copy-directory/${folderName}`, err => {});
         }
     });
+    console.log("'files-copy' created!");
+
 };
 
-function deleteFiles(path) {
-    fs.readdir(path, { encoding: "utf-8", withFileTypes: true }, (error, dirEntryList) => {
+async function deleteFiles(path) {
 
-        if (error) throw error;
-        dirEntryList.forEach(dirent => {
-    
-            fs.unlink(`${path}${dirent.name}`, err => {
-                if (err) throw err;
+    fs.readdir(path, { encoding: "utf-8", withFileTypes: true }, (error, dirEntryList) => {
+        if (!error) {
+            dirEntryList.forEach(dirent => {
+
+                fs.unlink(`${path}${dirent.name}`, err => {
+                    if (err) throw err;
+                });
+
             });
-    
-        });
+        }
     });
+    console.log("'files-copy' is cleared!");
+
 };
 
 function copyDir(pathFrom, pathTo) {
-    createNewFolder("files-copy");
-    deleteFiles("./04-copy-directory/files-copy/");
+
     fs.readdir("./04-copy-directory/files", { encoding: "utf-8", withFileTypes: true }, (error, dirEntryList) => {
+
         if (error) console.log(error);
         dirEntryList.forEach(item => {
+
             fs.copyFile(`${pathFrom}${item.name}`, `${pathTo}${item.name}`, err => {
                 if (err) console.log(err);
-                console.log(`"${item.name}" has been copied or updated!`);
-            })
+            });
+
         });
+
     });
+    console.log("Files coppied!");
+
 };
+
+createNewFolder("files-copy");
+deleteFiles("./04-copy-directory/files-copy/");
 copyDir("./04-copy-directory/files/", "./04-copy-directory/files-copy/");
